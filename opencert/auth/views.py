a58d730d7dc5f8166bb0e853df1e184b3a2736d1
GuanCong <guancong96@gmail.com>
2022-10-13 16:15:53 +0800

    wip: 2fa authentication

--- opencert/auth/views.py
+++ opencert/auth/views.py
@@ -4,7 +4,8 @@ from flask import Blueprint, render_template
 blueprint = Blueprint("auth", __name__, static_folder="../static")
 
 
-@blueprint.route("/authenticate/", methods=["GET"])
+@blueprint.route("/tf-validate/", methods=["GET, POST"])
 def two_auth():
     """List members."""
+    
     return render_template("auth/auth.html")
