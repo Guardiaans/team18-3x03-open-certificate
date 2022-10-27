5b9b30175b99dabb08e93578b6090c2f73f06eac
Escabas James Baldos <EscabasJames@hotmail.com>
2022-10-27 15:22:10 +0800

    Create user update page with basic functionality

--- opencert/user/forms.py
+++ opencert/user/forms.py
@@ -3,13 +3,13 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
+from flask_login import current_user
 
 from .models import User
 
 
 class RegisterForm(FlaskForm):
     """Register form."""
-
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=3, max=25)]
     )
@@ -50,5 +50,37 @@ class RegisterForm(FlaskForm):
         user = User.query.filter_by(email=self.email.data).first()
         if user:
             self.email.errors.append("Email already registered")
+            return False
+        return True
+
+class UpdateForm(FlaskForm):
+    """Register form."""
+
+    wallet_add = StringField(
+        "Wallet Address", validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    first_name = StringField(
+        "First Name", validators=[DataRequired(), Length(min=1, max=20)]
+    )
+    last_name = StringField(
+        "Last Name", validators=[DataRequired(), Length(min=1, max=20)]
+    )
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        "Verify password",
+        [DataRequired(), EqualTo("password", message="Passwords must match")],
+    )
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(UpdateForm, self).__init__(*args, **kwargs)
+        self.user = None
+
+    def validate(self):
+        """Validate the form."""
+        initial_validation = super(UpdateForm, self).validate()
+        if not initial_validation:
             return False
         return True
--- opencert/user/views.py
+++ opencert/user/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_required, current_user
+from opencert.user.models import User
+from opencert.user.forms import UpdateForm
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
@@ -11,3 +13,20 @@ blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../s
 def members():
     """List members."""
     return render_template("users/members.html")
+
+@blueprint.route("/update", methods=["GET", "POST"])
+@login_required
+def update():
+    """Update user information"""
+    form = UpdateForm(request.form)
+    user = User.query.filter_by(username=current_user.username).first_or_404()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            user.wallet_add = form.wallet_add.data
+            user.first_name = form.first_name.data
+            user.last_name = form.last_name.data
+            user.password = form.password.data
+            User.update(user)
+            flash("Account has successfully been updated.", "success")
+            return redirect(url_for("user.members"))
+    return render_template("users/updateuser.html", form=form)
