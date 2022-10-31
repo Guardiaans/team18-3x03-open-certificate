6024958e44c9cb2e34926fd562b402430922ce3f
2000859YangXiao <2000859@sit.singaporetech.edu.sg>
2022-10-31 19:51:29 +0800

    refactor: added login security

--- opencert/transfer/views.py
+++ opencert/transfer/views.py
@@ -1,32 +1,28 @@
 # -*- coding: utf-8 -*-
 """Public section, including homepage and signup."""
 from flask import Blueprint, render_template
-
+from flask_login import login_required
 from opencert.extensions import login_manager
 from opencert.user.models import User
 
 blueprint = Blueprint("transfer", __name__, static_folder="../static")
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    """Load user by ID."""
-    return User.get_by_id(int(user_id))
-
-
 @blueprint.route("/transfer", methods=["GET", "POST"])
+@login_required
 def transfer():
     """Transfer certificate page."""
     return render_template("transfer/transfer.html")
 
 
 @blueprint.route("/transferfail", methods=["GET"])
+@login_required
 def transferfail():
     """Transfer failed page."""
     return render_template("transfer/transferfail.html")
 
 
 @blueprint.route("/transfersuccess", methods=["GET"])
+@login_required
 def transfersucces():
     """Transfer succeeded page."""
     return render_template("transfer/transfersuccess.html")
