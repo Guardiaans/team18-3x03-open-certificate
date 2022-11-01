0309afd4b4c2778d2a6de81bd74e9d0178948de7
eddieson1997 <2000687@sit.singaporetech.edu.sg>
2022-11-01 14:47:43 +0800

    fix: change smart contract,add login require, home

--- opencert/transfer/views.py
+++ opencert/transfer/views.py
@@ -4,29 +4,28 @@ from flask import Blueprint, render_template
 
 from opencert.extensions import login_manager
 from opencert.user.models import User
+from flask_login import login_required
 
 blueprint = Blueprint("transfer", __name__, static_folder="../static")
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    """Load user by ID."""
-    return User.get_by_id(int(user_id))
-
 
 @blueprint.route("/transfer", methods=["GET", "POST"])
+@login_required
 def transfer():
     """Transfer certificate page."""
     return render_template("transfer/transfer.html")
 
 
 @blueprint.route("/transferfail", methods=["GET"])
+@login_required
 def transferfail():
     """Transfer failed page."""
     return render_template("transfer/transferfail.html")
 
 
 @blueprint.route("/transfersuccess", methods=["GET"])
+@login_required
 def transfersucces():
     """Transfer succeeded page."""
     return render_template("transfer/transfersuccess.html")
--- opencert/verify/views.py
+++ opencert/verify/views.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 """Public section, including homepage and signup."""
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 blueprint = Blueprint("verify", __name__, static_folder="../static")
 
 
 @blueprint.route("/verify", methods=["GET", "POST"])
-# @login_required
+@login_required
 def verify():
     """Verify NFT Page."""
     return render_template("verify/verifyNFT.html")
 
 
 @blueprint.route("/verifyfail", methods=["GET"])
+@login_required
 def verifyfail():
     """Verify failed page."""
     return render_template("verify/verifyfail.html")
 
 
 @blueprint.route("/verifysuccess", methods=["GET"])
+@login_required
 def verifysucces():
     """Verify succeeded page."""
     return render_template("verify/verifysuccess.html")
